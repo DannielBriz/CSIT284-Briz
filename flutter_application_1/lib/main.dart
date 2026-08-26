c03a363f12de0d7efea91b734ffd5002f723d8fb
LAB12_USER ON WS08 <L12X16W08@LabHCI.net>
2026-08-26 16:42:33 +0800

    Implement roll dice

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,33 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/dice_roller.dart';
 
 void main() {
-      runApp(
-        MaterialApp(
-          home: Scaffold(
-            body: Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(colors: [
-                  Colors.cyan,
-                  Colors.pink
-                ])
-              ),
-              child: Center(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Image.asset(
-                      width: 200,
-                    'assets/dice-images/dice-2.png'),
-                    TextButton(onPressed:() {},
-                    child: Text(
-                      style:TextStyle,(
-                        fontSize: 28
-                        )
-                      )
-                    )
-                  ],
-                ),
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [Colors.cyan, Colors.pink]),
           ),
+          child: Center(child: DiceRoller()),
         ),
       ),
     ),
